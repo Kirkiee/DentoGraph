83770e4f45000ca5916ef4b4e639b8dd3fe993b5
User@LAPTOP-CMEFHP1R <justinkirkendozo@gmail.com>
2026-06-07 16:06:30 +0800

    server edit for web hosting

--- backend/server.js
+++ backend/server.js
@@ -56,3 +56,9 @@ app.use("/api/audit-logs", auditLogRoutes);
 app.listen(5000, () => {
   console.log("Server running on port 5000");
 });
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
